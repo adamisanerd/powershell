cacc069343a4b1dda435f055de8dafdab2d41cdd
Adam Runion <adamrunion1@gmail.com>
2018-07-20 13:24:17 -0400

    Readability

--- build_plan.ps1
+++ build_plan.ps1
@@ -1,13 +1,16 @@
 # Get device drivers
-Get-WmiObject Win32_SystemDriver | Select-Object @{Name="Device Drivers";Expression={$_.DisplayName}}, Name,@{n="Version";e={(Get-Item $_.pathname).VersionInfo.FileVersion}} | Export-Csv drivers.csv -NoTypeInformation
+Get-WmiObject Win32_SystemDriver | 
+Select-Object @{Name="Device Drivers";Expression={$_.DisplayName}}, Name,@{n="Version";e={(Get-Item $_.pathname).VersionInfo.FileVersion}} | 
+Export-Csv drivers.csv -NoTypeInformation
 
 # Get Services
-#Get-WmiObject -Class win32_service | Select-Object DisplayName, StartMode, State | Export-Csv services.csv -NoTypeInformation
-Get-WmiObject -Class win32_service | Select-Object @{Name="Services/Daemons";Expression={$_.DisplayName}}, StartMode | Export-Csv services.csv -NoTypeInformation
+Get-WmiObject -Class win32_service | 
+Select-Object @{Name="Services/Daemons";Expression={$_.DisplayName}}, StartMode | 
+Export-Csv services.csv -NoTypeInformation
 
 # Get Installed Hotfixes, Applications, and Updates
-#Get-Hotfix | Select-Object CSName, HotFixID, Description | Export-Csv Hotfixes.csv -NoTypeInformation
-Get-Hotfix | Select-Object @{Name="Computer Name";Expression={$_.CSName}}, HotFixID, Description | Export-Csv Hotfixes.csv -NoTypeInformation
+Get-Hotfix | Select-Object @{Name="Computer Name";Expression={$_.CSName}}, HotFixID, Description | 
+Export-Csv Hotfixes.csv -NoTypeInformation
 
 Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*  |
 Select-Object DisplayName, DisplayVersion,
